fb49618a17170c6c679001cf95d871d7fdaf963b
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2021-05-29 13:47:49 +0200

    hero name fade-in animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,12 @@
+//first-name & last-name slide-in animation
+//subtitle fade-in animation
+gsap.from(".first-name", {duration: 1, x: 200, opacity: 0, ease: "power3.out", delay: 0.75});
+gsap.from(".last-name", {duration: 1, x: -100, opacity: 0, ease: "power3.out", delay: 0.5});
+gsap.to(".overlay", {duration: 1, scaleX: 0, delay: 2 });
+
+
+// ##################################
+
 // show nav by clicking burger-btn on mobile devices
 const burger = document.getElementById('burger');
 const navList = document.getElementById('nav-list');
@@ -5,19 +14,23 @@ let navOpen = false;
 
 
 const toggleNav = () => {
-    if (navOpen) {
-        burger.classList.remove('btn-active');
-        navList.classList.remove('nav-open');
-        navOpen = false;
-    } else {
-        burger.classList.add('btn-active');
-        navList.classList.add('nav-open');
-        fadeIn();
-        navOpen = true;
-    }
+    navOpen ? closeNav() : openNav()
+}
+
+const closeNav = () => {
+    burger.classList.remove('btn-active');
+    navList.classList.remove('nav-open');
+    navOpen = false;
+}
+
+const openNav = () => {
+    burger.classList.add('btn-active');
+    navList.classList.add('nav-open');
+    fadeIn();
+    navOpen = true;
 }
 // animates nav__links sliding and fading in
-const fadeIn = () => {    
+const fadeIn = () => {
     gsap.from(".list-item", { duration: 0.5, x: 50, opacity: 0.1, delay: 0.3, ease: "power3.out", stagger: 0.15 });
 }
 
@@ -30,14 +43,15 @@ burger.addEventListener('click', toggleNav);
 const links = [...document.getElementsByClassName('nav__link')];
 
 links.forEach(link => {
-    link.addEventListener('click', function(evt) {
+    link.addEventListener('click', function (evt) {
         removeActiveClass();
         const border = evt.target.nextSibling;
-        gsap.to(border, {opacity: 1});
-        gsap.to(border, {y: -35, duration: 0.3});
-        gsap.to(border, {opacity: 0, y: 0, delay: 0.5});
+        gsap.to(border, { opacity: 1 });
+        gsap.to(border, { y: -35, duration: 0.3 });
+        gsap.to(border, { opacity: 0, y: 0, delay: 0.5 });
         evt.target.classList.add('nav__link--active');
-    })    
+        closeNav();
+    })
 });
 
 const removeActiveClass = () => {
@@ -45,3 +59,8 @@ const removeActiveClass = () => {
         link.classList.remove('nav__link--active');
     })
 }
+
+
+// ###################################
+
+
