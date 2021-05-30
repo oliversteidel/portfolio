dae4ae40aef252673446c9ca2727adb4106d5a49
oliversteidel <67387391+oliversteidel@users.noreply.github.com>
2021-05-30 17:15:08 +0200

    created portfolio section

--- src/js/main.js
+++ src/js/main.js
@@ -63,32 +63,85 @@ const removeActiveClass = () => {
 
 // ###################################
 
+// init gsap.timelines for each "tech"-element and run these timelines with moveItem()
+const moveTechItems = () => {        
+    let tech1 = gsap.timeline({ repeat: 5, delay: 2 });
+    let tech2 = gsap.timeline({ repeat: 5, delay: 4 });
+    let tech3 = gsap.timeline({ repeat: 5, delay: 6 });
+    let tech4 = gsap.timeline({ repeat: 5, delay: 8 });
+    let tech5 = gsap.timeline({ repeat: 5, delay: 10 });
+    let tech6 = gsap.timeline({ repeat: 5, delay: 12 });
 
+    moveItem(tech1, ".tech1");
+    moveItem(tech2, ".tech2");
+    moveItem(tech3, ".tech3");
+    moveItem(tech4, ".tech4");
+    moveItem(tech5, ".tech5");
+    moveItem(tech6, ".tech6");
 
-const moveTechItems = () => {
-    let move = gsap.timeline({repeat: 5, delay: 0.5 });
-    let move1 = gsap.timeline({repeat: 5, delay: 1 });
-    let move2= gsap.timeline({repeat: 5, delay: 1.5 });
-    let move3 = gsap.timeline({repeat: 5, delay: 2 });
-    let move4 = gsap.timeline({repeat: 5, delay: 2.5 });
-    let move5 = gsap.timeline({repeat: 5, delay: 3 });
-    
-    moveItem(move, ".tech1");
-    moveItem(move1, ".tech2");
-    moveItem(move2, ".tech3");
-    moveItem(move3, ".tech4");
-    moveItem(move4, ".tech5");
-    moveItem(move5, ".tech6");
+    const techItems = [...document.getElementsByClassName('tech')];
+
+    techItems.forEach(item => {
+        item.addEventListener('mouseover', function (e) {
+            
+            switch (getTimelineFromClassName(e.target.className)) {
+                case 'tech1':
+                    tech1.pause();                    
+                    break;
+                case 'tech2':
+                    tech2.pause();
+                    break;
+                case 'tech3':
+                    tech3.pause();
+                    break;
+                case 'tech4':
+                    tech4.pause();
+                    break;
+                case 'tech5':
+                    tech5.pause();
+                    break;
+                case 'tech6':
+                    tech6.pause();
+                    break;
+            }
+        })
+
+        item.addEventListener('mouseout', function(e) {
+
+            switch (getTimelineFromClassName(e.target.className)) {
+                case 'tech1':
+                    tech1.resume();
+                    break;
+                case 'tech2':
+                    tech2.resume();
+                    break;
+                case 'tech3':
+                    tech3.resume();
+                    break;
+                case 'tech4':
+                    tech4.resume();
+                    break;
+                case 'tech5':
+                    tech5.resume();
+                    break;
+                case 'tech6':
+                    tech6.resume();
+                    break;
+
+            }
+        })
+    })
 }
 
+// configs timeline(arg1) animation for element(arg2); creates random coordinates 2 times
 const moveItem = (timeline, element) => {
-    timeline.to(element, {duration: 2, opacity: 1});
-    let [posX, posY] = setRandomCoordinates(0, 500);    
-    timeline.to(element, {duration: 10, x: posX, y: posY});
+    timeline.to(element, { duration: 2, opacity: 0.2 });
+    let [posX, posY] = setRandomCoordinates(0, 500);
+    timeline.to(element, { duration: 10, x: posX, y: posY });
     [posX, posY] = setRandomCoordinates(-300, 0);
-    timeline.to(element, {duration: 10, x: posX, y: posY});
-    timeline.to(element, {duration: 10, x: 0, y: 0});
-    timeline.to(element, {duration: 1, opacity: 0});
+    timeline.to(element, { duration: 10, x: posX, y: posY });
+    timeline.to(element, { duration: 10, x: 0, y: 0 });
+    timeline.to(element, { duration: 1, opacity: 0 });
 }
 
 // returns a random number between min & max
@@ -97,9 +150,19 @@ const setRandomCoordinates = (min, max) => {
     max = Math.floor(max);
     let posX = Math.floor(Math.random() * (max - min)) + min;
     let posY = Math.floor(Math.random() * (max - min)) + min;
-    console.log(`x:${posX}, y:${posY}`);
     return [posX, posY];
+}
 
+const getTimelineFromClassName = (str) => {
+    let timeline = str.substring(5, 10);
+    return timeline;
 }
 
 moveTechItems();
+
+
+
+
+
+
+
